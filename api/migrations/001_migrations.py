17af94bf98d31bfac4a5a391e4de9eca57a38baa
JoeFerraro <jferraro262@gmail.com>
2023-05-19 19:47:57 -0700

    FK working

--- api/migrations/001_migrations.py
+++ api/migrations/001_migrations.py
@@ -1,21 +1,8 @@
 steps = [
     [
-        # "Up" SQL statement
-        """
-        CREATE TABLE sales (
-            id SERIAL PRIMARY KEY NOT NULL,
-            quanity INT NOT NULL
-        );
-        """,
-        # "Down" SQL statement
-        """
-        DROP TABLE sales;
-        """,
-    ],
-    [
         """
         CREATE TABLE states (
-            id SERIAL PRIMARY KEY NOT NULL,
+            state_id SERIAL PRIMARY KEY NOT NULL,
             state_name VARCHAR(100) NOT NULL
         );
         """,
@@ -27,12 +14,12 @@ steps = [
     [
         """
         CREATE TABLE accounts (
-            id SERIAL PRIMARY KEY NOT NULL,
+            account_id SERIAL PRIMARY KEY NOT NULL,
             username VARCHAR(50) NOT NULL UNIQUE,
             password VARCHAR(255) NOT NULL,
             avatar_img VARCHAR(254),
             email VARCHAR(254) NOT NULL UNIQUE,
-            event_manager BOOLEAN
+            event_manager BOOLEAN DEFAULT false
         );
         """,
         """
@@ -40,10 +27,25 @@ steps = [
         """,
     ],
     [
+        # "Up" SQL statement
+        """
+        CREATE TABLE sales (
+            sale_id SERIAL PRIMARY KEY NOT NULL,
+            event INT REFERENCES events(event_id)
+            quanity INT NOT NULL,
+            sold_to INT REFERENCES accounts(account_id)
+        );
+        """,
+        # "Down" SQL statement
+        """
+        DROP TABLE sales;
+        """,
+    ],
+    [
         ##Create events table
         """
         CREATE TABLE events (
-            id SERIAL PRIMARY KEY NOT NULL,
+            event_id SERIAL PRIMARY KEY NOT NULL,
             event_name VARCHAR(100) NOT NULL,
             event_image TEXT,
             event_type VARCHAR(100) NOT NULL,
@@ -56,7 +58,10 @@ steps = [
             tickets_price DECIMAL(10,2),
             promoted BOOLEAN DEFAULT false,
             venue VARCHAR(100),
-            city VARCHAR(100)
+            city VARCHAR(100),
+            state_id INT REFERENCES states(state_id),
+            created_by INT REFERENCES accounts(account_id)
+
         );
         """,
         ##Drop the events table
@@ -65,5 +70,3 @@ steps = [
         """,
     ],
 ]
-#  created_by VARCHAR(100) REFERENCES accounts(username),
-# state VARCHAR(100) REFERENCES state(state_name)
