18d602fbb5373e471488d4cafdd98524da10a182
Michael Gianoulakis <mgiano02@gmail.com>
2023-05-25 15:33:13 -0700

    Added state change and event handlers to cart page

--- ghi/src/sales/cart.js
+++ ghi/src/sales/cart.js
@@ -42,7 +42,7 @@ function Cart() {
         const response = await fetch(saleUrl, FetchConfig);
         if (response.ok) {
             const newSale = await response.json();
-
+            console.log(newSale);
             setEvent("");
             setQuantity("");
             setSoldTo("");
@@ -57,43 +57,36 @@ function Cart() {
             <div className="container mx-auto">
             <h5 className="flex justify-center">Cart Checkout</h5>
             <div className="flex justify-center">
-                <form className="grid-cols-2">
+                <form onSubmit={handleSubmit} className="grid-cols-2">
                     <div className="flex flex-col space-y-1">
-                        <label className="text-sm font-semibold text-gray-500">
-                        Event:
-                        </label>
+                        <label className="text-sm font-semibold text-gray-500">Event:</label>
                         <input
+                        onChange={handleEventChange}
                         name="event"
                         type="text"
-                        className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
-                        />
+                        className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"/>
                     </div>
                     <div className="flex flex-col space-y-1">
-                        <label className="text-sm font-semibold text-gray-500">
-                        Quantity:
-                        </label>
+                        <label className="text-sm font-semibold text-gray-500">Quantity:</label>
                         <input
+                        onChange={handleQuantityChange}
                         name="quantity"
                         type="number"
                         placeholder="0"
-                        className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
-                        />
+                        className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"/>
                     </div>
                     <div className="flex flex-col space-y-1">
-                        <label className="text-sm font-semibold text-gray-500">
-                        Sold To:
-                        </label>
+                        <label className="text-sm font-semibold text-gray-500">Sold To:</label>
                         <input
+                        onChange={handleSoldTo}
                         name="sold-to"
                         type="text"
-                        className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"
-                        />
+                        className="px-4 py-2 transition duration-300 border border-gray-300 rounded focus:border-transparent focus:outline-none focus:ring-4 focus:ring-blue-200"/>
                     </div>
                     <div className="flex justify-end">
                         <button
                         className="w-full px-4 py-2 text-lg font-semibold text-white transition-colors duration-300 bg-blue-500 rounded-md shadow hover:bg-blue-600 focus:outline-none focus:ring-blue-200 focus:ring-4"
-                        type="submit"
-                        >
+                        type="submit">
                         Checkout
                         </button>
                     </div>
