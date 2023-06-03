9fa3c0882719c3e0814a983a9471a72507d9f748
JoeFerraro <jferraro262@gmail.com>
2023-06-02 17:41:34 -0700

    car-O-sell works

--- ghi/src/MainPage.js
+++ ghi/src/MainPage.js
@@ -5,9 +5,10 @@ import dayjs from "dayjs";
 import { FiChevronsLeft, FiChevronsRight } from "react-icons/fi";
 
 function MainPage() {
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [leftIndex, setLeftIndex] = useState(0);
+  const [rightIndex, setRightIndex] = useState(2);
   const [events, setEvents] = useState([]);
-  const [hover, setHover] = useState(true);
 
   useEffect(() => {
     async function getEventData() {
@@ -25,31 +26,79 @@ function MainPage() {
   }, []);
 
   const leftClick = () => {
-    if (currentIndex === 0) {
+    if (leftIndex === 0) {
+      setCurrentIndex(currentIndex - 1);
+      setLeftIndex(events.length - 1);
+      setRightIndex(rightIndex - 1);
+    } else if (currentIndex === 0) {
       setCurrentIndex(events.length - 1);
+      setLeftIndex(leftIndex - 1);
+      setRightIndex(rightIndex - 1);
+    } else if (rightIndex === 0) {
+      setCurrentIndex(currentIndex - 1);
+      setLeftIndex(leftIndex - 1);
+      setRightIndex(events.length - 1);
     } else {
       setCurrentIndex(currentIndex - 1);
+      setLeftIndex(leftIndex - 1);
+      setRightIndex(rightIndex - 1);
     }
   };
 
   const rightClick = () => {
-    if (currentIndex === events.length - 1) {
+    if (rightIndex === events.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      setLeftIndex(leftIndex + 1);
+      setRightIndex(0);
+    } else if (currentIndex === events.length - 1) {
       setCurrentIndex(0);
+      setLeftIndex(leftIndex + 1);
+      setRightIndex(rightIndex + 1);
+    } else if (leftIndex === events.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+      setLeftIndex(0);
+      setRightIndex(rightIndex + 1);
     } else {
       setCurrentIndex(currentIndex + 1);
+      setLeftIndex(leftIndex + 1);
+      setRightIndex(rightIndex + 1);
     }
   };
+
+  const goToEvent()
 
   return (
     <>
       {events ? (
         <div className="grid grid-cols-5">
+          <div className="flex flex-col justify-center content-center col-start-1 col-span-1 mt-12">
+            <div className="grid grid-cols-4 grid-rows-5 m-2">
+              <div className="relative shadow-xl shadow-blue-500 border-4 border-blue-500 rounded-3xl overflow-hidden aspect-video col-start-1 col-span-4 row-start-1 row-span-5">
+                <img
+                  className="absolute top-0 left-0 object-cover opacity-50 w-full h-full"
+                  src={events[rightIndex]?.event_image}
+                />
+              </div>
+              <div className="flex z-0 justify-center items-center col-start-1 col-span-4 row-start-1 row-span-1">
+                <h1 className="text-xs font-semibold">
+                  {events[rightIndex]?.event_name}
+                </h1>
+              </div>
+              <div className="flex z-0 justify-center items-center col-start-1 col-span-2 row-start-5 row-span-1">
+                <h1 className="text-xs font-semibold">
+                  {events[rightIndex]?.city}, {events[rightIndex]?.state_id}{" "}
+                </h1>
+              </div>
+              <div className="flex z-0 justify-center items-center col-start-3 col-span-2 row-start-5 row-span-1">
+                <h1 className="text-xs font-semibold">
+                  {dayjs(events[rightIndex]?.date).format("MM-DD-YY")}
+                </h1>
+              </div>
+            </div>
+          </div>
           <div className="flex flex-col justify-center content-center col-start-2 col-span-3 mt-2.5">
             <h1 className="text-7xl font-bold text-center">PassPro</h1>
-            <div
-              onMouseEnter={() => setHover(false)}
-              className="grid grid-cols-4 grid-rows-5 m-2"
-            >
+            <div onClick={} className="grid grid-cols-4 grid-rows-5 m-2">
               <div className="relative shadow-xl shadow-blue-500 border-4 border-blue-500 rounded-3xl overflow-hidden aspect-video col-start-1 col-span-4 row-start-1 row-span-5">
                 <img
                   className="absolute top-0 left-0 object-cover opacity-50 w-full h-full"
@@ -81,7 +130,6 @@ function MainPage() {
               </div>
               <div className="z-0 flex justify-end items-center col-start-4 col-span-1 row-start-3 row-span-1">
                 <button
-                  hidden={hover}
                   className="text-5xl hover:text-blue-500"
                   onClick={rightClick}
                 >
@@ -90,6 +138,34 @@ function MainPage() {
               </div>
             </div>
             <p className="text-center">Make that Money</p>
+          </div>
+          <div className="flex flex-col justify-center content-center col-start-5 col-span-1 mt-12">
+            <div className="grid grid-cols-4 grid-rows-5 m-2">
+              <div className="relative shadow-xl shadow-blue-500 border-4 border-blue-500 rounded-3xl overflow-hidden aspect-video col-start-1 col-span-4 row-start-1 row-span-5">
+                <img
+                  className="absolute top-0 left-0 object-cover opacity-50 w-full h-full"
+                  src={events[leftIndex]?.event_image}
+                />
+              </div>
+              <div className="flex z-0 justify-center items-center col-start-1 col-span-4 row-start-1 row-span-1">
+                <h1 className="text-xs font-semibold">
+                  {events[leftIndex]?.event_name}
+                </h1>
+              </div>
+              <div className="flex z-0 justify-center items-center col-start-1 col-span-2 row-start-5 row-span-1">
+                <h1 className="text-xs font-semibold">
+                  {events[leftIndex]?.city}, {events[leftIndex]?.state_id}{" "}
+                </h1>
+              </div>
+              <div className="flex z-0 justify-center items-center col-start-3 col-span-2 row-start-5 row-span-1">
+                <h1 className="text-xs font-semibold">
+                  {dayjs(events[leftIndex]?.date).format("MM-DD-YY")}
+                </h1>
+              </div>
+            </div>
+          </div>
+          <div className="flex border col-start-1 col-span-5 mt-12">
+            <p>stuff here</p>
           </div>
         </div>
       ) : (
