2f095844abdfe1acf107ac648909d49896866565
JoeFerraro <jferraro262@gmail.com>
2023-06-07 16:55:53 -0700

    intial

--- api/routers/sales_router.py
+++ api/routers/sales_router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, Response
 from typing import List, Union
 from queries.sales_queries import SalesIn, SalesOut, SaleRepository, Error
+from authenticator import authenticator
 
 
 router = APIRouter()
@@ -8,7 +9,10 @@ router = APIRouter()
 
 @router.post("/api/sales", response_model=Union[SalesOut, Error])
 def create_sale(
-    sale: SalesIn, response: Response, repo: SaleRepository = Depends()
+    sale: SalesIn,
+    response: Response,
+    account: dict = Depends(authenticator.get_current_account_data),
+    repo: SaleRepository = Depends(),
 ):
     if response:
         return repo.create(sale)
@@ -19,7 +23,8 @@ def create_sale(
 
 @router.get("/api/sales", response_model=Union[List[SalesOut], Error])
 def list_sales(
-    response: Response, repo: SaleRepository = Depends(),
+    response: Response,
+    repo: SaleRepository = Depends(),
 ):
     if response:
         return repo.list_sales()
