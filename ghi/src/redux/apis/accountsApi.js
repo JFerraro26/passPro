32c478b9a316461249418bacb651edf100f12f6d
JoeFerraro <jferraro262@gmail.com>
2023-06-07 19:09:12 -0700

    removed unused functions for pipeline

--- ghi/src/redux/apis/accountsApi.js
+++ ghi/src/redux/apis/accountsApi.js
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { useSelector } from "react-redux";
 
 export const accountsApi = createApi({
   reducerPath: "authentication",
