79d21c2c5de026cdae73fb3cc2fd3b0bd64aaf09
Chad Manuel <chadc_manuel@yahoo.com>
2023-06-09 10:55:09 -0700

    added an alert inside of the cart when not logged in

--- ghi/src/nav/LoginPopUp.js
+++ ghi/src/nav/LoginPopUp.js
@@ -3,50 +3,52 @@ import LoginForm from "../accounts/loginForm";
 import SignUpForm from "../accounts/signUpForm";
 
 function LogInPopUP() {
-  const [open, setOpen] = useState(false);
-  const [login, setLogin] = useState(false);
-  return (
-    <div className="flex justify-end">
-      <button
-        className="relative block text-2xl font-semibold hover:text-red-500"
-        onClick={() => setOpen(true)}
-      >
-        Login
-      </button>
-      {open ? (
-        <div className="fixed z-10 top-0 left-0 h-full w-full">
-          <div className="m-16 grid grid-cols-5 grid-rows-5">
-            <div className="bg-orange-100 flex flex-col col-start-2 col-span-3 row-start-1 row-span-3 border-4 border-blue-500 rounded-2xl mt-16">
-              <div className="m-8">
-                <button onClick={() => setOpen(false)}>exit</button>
-                {login ? (
-                  <div className="flex flex-col">
-                    <SignUpForm setOpen={setOpen} />
-                    <button
-                      onClick={() => setLogin(!login)}
-                      className="mt-4 underline hover:text-blue-500 transition-all"
-                    >
-                      Already have an account? Click Here!
-                    </button>
-                  </div>
-                ) : (
-                  <div className="flex flex-col">
-                    <LoginForm setOpen={setOpen} />
-                    <button
-                      onClick={() => setLogin(!login)}
-                      className="mt-4 underline hover:text-blue-500 transition-all"
-                    >
-                      Need an account? Sign Up Here!
-                    </button>
-                  </div>
-                )}
-              </div>
-            </div>
-          </div>
+    const [open, setOpen] = useState(false);
+    const [login, setLogin] = useState(false);
+    return (
+        <div className="flex justify-end items-start">
+            <button
+                className="relative block text-2xl font-semibold hover:text-red-500"
+                onClick={() => setOpen(true)}
+            >
+                Login
+            </button>
+            {open ? (
+                <div className="fixed z-10 top-0 left-0 h-full w-full">
+                    <div className="m-16 grid grid-cols-5 grid-rows-5">
+                        <div className="bg-orange-100 flex flex-col col-start-2 col-span-3 row-start-1 row-span-3 border-4 border-blue-500 rounded-2xl mt-16">
+                            <div className="m-8">
+                                <button onClick={() => setOpen(false)}>
+                                    exit
+                                </button>
+                                {login ? (
+                                    <div className="flex flex-col">
+                                        <SignUpForm setOpen={setOpen} />
+                                        <button
+                                            onClick={() => setLogin(!login)}
+                                            className="mt-4 underline hover:text-blue-500 transition-all"
+                                        >
+                                            Already have an account? Click Here!
+                                        </button>
+                                    </div>
+                                ) : (
+                                    <div className="flex flex-col">
+                                        <LoginForm setOpen={setOpen} />
+                                        <button
+                                            onClick={() => setLogin(!login)}
+                                            className="mt-4 underline hover:text-blue-500 transition-all"
+                                        >
+                                            Need an account? Sign Up Here!
+                                        </button>
+                                    </div>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : null}
         </div>
-      ) : null}
-    </div>
-  );
+    );
 }
 
 export default LogInPopUP;
